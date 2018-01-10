b54f7010f48b9e3b6861bf2b68c896492649e702
dingjunjie <dingjunjie@21tb.com>
2018-01-10 16:13:34 +0800

    测试

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 $(function () {
+    if (window.devicePixelRatio > 1) {
+        var images = $('#wx-background');
+        var lowres = images.attr('src', highres);
+        var highres = lowres.replace(".", "@2x.");
+        $(images).attr('src', highres);
+    }
+
     $('ytd-expander').each(function (i, v) {
         if ($(v).find("#content-text").height() > $(v).height()) {
             $(v).find("#less").attr("hidden", true);
